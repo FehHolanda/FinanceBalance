a3b426ba9a597893436b02b155f691a370ad4e5b
Felipe Holanda <felipeholl@hotmail.com>
2023-12-08 22:39:35 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { Express } from "express";
+import express from "express";
+import { config } from "dotenv";
 
-const app = Express();
 
-c
+config();
 
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port,() => console.log(`listening on port ${port}!`));
+
+app.get("/",(req,res)=> {res.send("Hello World");});
