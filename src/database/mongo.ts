273209f4183f8803624fe28f05fdd2571ebfcaab
Felipe Holanda <felipeholl@hotmail.com>
2023-12-13 23:39:57 -0300

    ops :  string connect build db

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -9,7 +9,8 @@ export const MongoClient = {
         //const username = process.env.MONGODB_USERNAME || "root";
         //const password = process.env.MONGODB_PASSWORD || "password";
 
-        const client = new MongoCLI(url);
+        const client = new MongoCLI("mongodb+srv://doadmin:keS05734gB6Axh91@db-mongodb-finance-balance-06fc0690.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-finance-balance");
+        
         const db = client.db("users-db");
 
         this.client = client;
